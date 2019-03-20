758b778241e5be3f7551cc33f45fb835ad87ecaa
Tanver Ahmed Likhon <39611778+TanverLikhon@users.noreply.github.com>
2019-03-20 09:46:18 +0600

    dp

---
+++ xon.cpp
@@ -0,0 +1,41 @@
+/* A Naive recursive implementation of 0-1 Knapsack problem */
+#include<bits/stdc++.h>
+using namespace std;
+
+// A utility function that returns maximum of two integers
+int max(int a, int b) { return (a > b)? a : b; }
+
+// Returns the maximum value that can be put in a knapsack of capacity W
+int knapSack(int W, int wt[], int val[], int n)
+{
+// Base Case
+if (n == 0 || W == 0)
+	return 0;
+
+// If weight of the nth item is more than Knapsack capacity W, then
+// this item cannot be included in the optimal solution
+if (wt[n-1] > W)
+	return knapSack(W, wt, val, n-1);
+
+// Return the maximum of two cases:
+// (1) nth item included
+// (2) not included
+else{
+cout<<"W  "<<W<<" N "<<n<<endl;
+       int v=val[n-1]+knapSack(W-wt[n-1],wt,val,n-1);
+       int v1=knapSack(W, wt, val, n-1);
+       cout<<v<<"  "<<v1<<"    M  "<<max(v,v1)<<endl;
+        return max(v,v1
+				);}
+}
+
+// Driver program to test above function
+int main()
+{
+	int val[] = {10,40,50,75};
+	int wt[] = {5,10,3,12};
+	int W = 20;
+	int n = sizeof(val)/sizeof(val[0]);
+	printf("%d", knapSack(W, wt, val, n));
+	return 0;
+}
